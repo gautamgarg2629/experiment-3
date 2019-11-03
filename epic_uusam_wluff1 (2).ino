4d13dc5a1e1e9e701512ac116d3ec49ba8c35948
gautamgarg2629 <56790242+gautamgarg2629@users.noreply.github.com>
2019-11-03 12:25:23 -0800

    Add files via upload

---
+++ epic_uusam_wluff1 (2).ino
@@ -0,0 +1,24 @@
+void setup()
+{
+  for(int i=10; i<=13;i++){
+    pinMode(i,OUTPUT);
+  }
+}
+void loop()
+{
+  for(int i =10;i<14;i++){
+    
+    if(i!=13)
+{
+  digitalWrite(i, HIGH);
+  
+  digitalWrite(i+1, LOW);
+  delay(200); // Wait for 1000 millisecond(s)
+}
+else 
+    digitalWrite(i, HIGH);
+  
+  digitalWrite(i-3, LOW);
+  delay(200);\
+}
+}
